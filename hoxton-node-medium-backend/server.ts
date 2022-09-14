200f707260036ab383ad2e730b5f483c24e4eb47
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-09-14 14:59:35 +0200

    Add Post, Like, Comment done

--- hoxton-node-medium-backend/server.ts
+++ hoxton-node-medium-backend/server.ts
@@ -46,7 +46,7 @@ app.delete('/users/:id', async (req, res) => {
     })
     res.send({message: "User deleted"})
   })
-//get posts with likes, comments and the user who wrote them
+//get all the posts with likes, comments and the user who wrote them
 app.get('/posts', async(req, res)=>{
   const posts = await prisma.posts.findMany({include:{user: true, likes:true, comments:true}})
   res.send(posts)
@@ -71,6 +71,91 @@ app.delete('/posts/:id', async (req, res) => {
     where: { id }
   })
   res.send({message: "Post deleted"})
+})
+
+//create a Post
+app.post('/posts', async (req, res)=>{
+  const post = {
+    tittle:req.body.tittle,
+    blog:req.body.blog,
+    image:req.body.image,
+    userId: req.body.userId
+  }
+  let errors: string[] = []
+
+    if (typeof req.body.tittle !== 'string') {
+        errors.push('Add a proper Tittle!')
+      }
+    if (typeof req.body.blog !== 'string') {
+        errors.push('Add a proper Blog!')
+      }
+    if(typeof req.body.image  !=='string') {
+        errors.push('Add a proper URL')
+    }
+    if(typeof req.body.userId  !=='number') {
+        errors.push('Add a proper user ID')
+    }
+    if( errors.length === 0)  {
+  try{
+    const newPost = await prisma.posts.create({
+      data: {
+        tittle:post.tittle,
+        blog:post.blog,
+        image:post.image,
+        userId:post.userId
+      },
+      include: { likes: true, comments:true }
+    })
+    res.send(newPost)
+  } catch(err) {
+    // @ts-ignore
+    res.status(400).send(err.message)
+  }
+}else {
+  res.status(400).send({ errors: errors })
+}
+})
+//Like a Post
+app.post('/likePosts', async (req, res)=>{
+  const like = {
+    postId: req.body.postId
+  }
+  
+  try{
+    const LikePost = await prisma.likes.create({
+      data: {
+        postId:like.postId
+      },
+      include: { post:true }
+    })
+    res.send(LikePost)
+  } catch(err) {
+    // @ts-ignore
+    res.status(400).send(err.message)
+  }
+
+})
+//Comment a Post
+app.post('/commentPosts', async (req, res)=>{
+  const comment = {
+    postId: req.body.postId,
+    text:req.body.text
+  }
+  
+  try{
+    const CommentPost = await prisma.comments.create({
+      data: {
+        postId:comment.postId,
+        text:comment.text
+      },
+      include: { post:true }
+    })
+    res.send(CommentPost)
+  } catch(err) {
+    // @ts-ignore
+    res.status(400).send(err.message)
+  }
+
 })
 app.listen(port, () => {
     console.log(`http://localhost:${4000}`)
