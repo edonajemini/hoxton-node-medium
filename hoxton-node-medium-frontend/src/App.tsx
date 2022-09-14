a43dc9e26f52c2f239e4ffd85a5471c07cf2ce48
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-09-14 12:46:35 +0200

    first commit

---
+++ hoxton-node-medium-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>hoxton-node-medium-frontend</h1>
+    </div>
+  )
+}
+
+export default App
