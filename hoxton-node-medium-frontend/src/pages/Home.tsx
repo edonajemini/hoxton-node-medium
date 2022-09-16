fb85725d6e6f2fbaa2d727afa1f77a1414d6f066
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-09-16 19:46:26 +0200

    Finished the add post form

--- hoxton-node-medium-frontend/src/pages/Home.tsx
+++ hoxton-node-medium-frontend/src/pages/Home.tsx
@@ -1,133 +1,150 @@
-import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { Users } from '../components/Users'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Users } from "../components/Users";
 type Posts = {
-    id : number,
-    tittle: string,
-    blog: string,
-    image : string,
-    userId : number
-    likes: []
-    comments:[]
-  }
+  id: number;
+  tittle: string;
+  blog: string;
+  image: string;
+  userId: number;
+  likes: [];
+  comments: [];
+};
 type Comments = {
-    id: number
-    text: string
-    postId: number
-}
+  id: number;
+  text: string;
+  postId: number;
+};
 
-  export function Home(){
-    const [posts, setPosts] = useState<Posts[]>([])
-    useEffect(() => {
-        fetch("http://localhost:4000/posts")
-          .then((resp) => resp.json())
-          .then((postsFromServer) => setPosts(postsFromServer));
-      }, []);
-    
-const [likes, setLikes] = useState(0)
-    
-const [comments, setComments] = useState<Comments[]>([])
+export function Home() {
+  const [posts, setPosts] = useState<Posts[]>([]);
+  useEffect(() => {
+    fetch("http://localhost:4000/posts")
+      .then((resp) => resp.json())
+      .then((postsFromServer) => setPosts(postsFromServer));
+  }, []);
 
-    useEffect(() => {
-        fetch(`http://localhost:4000/comments/`)
-          .then((resp) => resp.json())
-          .then((commentsFromServer) => setComments(commentsFromServer));
-      }, []);
-  
-     
-    return(
-        <div>
-          < Users />
-            
-            <form onSubmit={event => 
-           {event.preventDefault()
-            const postsCopy = structuredClone(posts)
-              
-              let newPost = {
-                tittle: event.target.tittle.value,
-                blog: event.target.blog.value,
-                image: event.target.image.value,
-                userid: event.target.userid.value
-              }
-              postsCopy.push(newPost);
-              setPosts(postsCopy)
+  const [likes, setLikes] = useState(0);
 
-              event.target.reset();
-            }}>
-              <h2>Add Post</h2>
-                <input type='text' name="tittle" id="tittle" placeholder='Tittle?' required></input>
-                <textarea name='blog' id='blog' placeholder='Your Blog?' required></textarea>
-                <input type='url' name='image' id='image' placeholder='Image?'></input>
-                <input type='number' name="userid" id='userid' placeholder='user ID' required></input>
-                <button className='post-btn' onClick={(event)=>{
-    fetch("http://localhost:4000/posts",{
-      method: 'POST',
-      headers: {
-        'content-Type': 'application/json'
-      },
-      body: JSON.stringify({
+  const [comments, setComments] = useState<Comments[]>([]);
 
-        tittle: document.getElementById("tittle")?.value,
-        blog: document.getElementById("blog")?.value,
-        image: document.getElementById("image")?.value,
-        userid: document.getElementById("userid")?.value
-        
-      }
-      )
-    }) .then(resp => resp.json())
-    .then(postsfromserver => setPosts([...posts, postsfromserver]))
-    location.reload()
-  }}>POST</button>
-            </form>
-            
-            
-            {
-            posts.reverse().map(post => (
-                <>
-                <div className='posts'>
-                  <div link-div>
-            
-                  <Link to={`/posts/${post.id}`}>
-                  
-                  <div className='tittle'>
-                    <h3>{post.tittle}</h3>
-                    <p>User ID: {post.id}</p>
-                    <p>{post.blog}..</p>
-                    </div>
-                    <div className='image'>
-                    <img src={post.image} width="200px" />
-                    </div>
-                    </Link>
-                    </div>
-                    
-                    <div className='like-com-delete'>
-                    <h4> <button
-          onClick={() => {
-            fetch(`http://localhost:4000/likePosts`, {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({ postId: post.id }) ,
-            }).then((resp)=> resp.json()).then(()=> location.reload());
-          }}>❤️</button>{post.likes.length} likes</h4>
-                    <h4><button>💬</button>{post.comments.length} comments</h4>
-                      <button onClick={()=>{
-                      fetch(`http://localhost:4000/posts/${post.id}`,{
-                        method:"DELETE"
-                      }).then((resp) => resp.json())
-                      .then(() =>
-                    location.reload()
-                      )
-                  
-                    }}> DELETE </button>
-                      </div>
+  useEffect(() => {
+    fetch(`http://localhost:4000/comments/`)
+      .then((resp) => resp.json())
+      .then((commentsFromServer) => setComments(commentsFromServer));
+  }, []);
+
+  return (
+    <div className="home">
+      <Users />
+
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          let newPost = {
+            tittle: event.target.tittle.value,
+            blog: event.target.blog.value,
+            image: event.target.image.value,
+            userId: Number(event.target.userId.value),
+          };
+          fetch("http://localhost:4000/posts", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newPost),
+          }).then(() => {
+            fetch("http://localhost:4000/posts")
+              .then((resp) => resp.json())
+              .then((postsFromServer) => setPosts(postsFromServer));
+          });
+          event.target.reset();
+        }}
+      >
+        <input
+          type="text"
+          name="tittle"
+          id="tittle"
+          placeholder="Tittle?"
+          required
+        ></input>
+        <input
+          type="text"
+          name="blog"
+          id="blog"
+          placeholder="Blog?"
+          required
+        ></input>
+        <input
+          type="url"
+          name="image"
+          id="image"
+          placeholder="Image URL?"
+          required
+        ></input>
+        <input
+          type="number"
+          name="userId"
+          id="userId"
+          placeholder="user ID?"
+        ></input>
+        <button>POST</button>
+      </form>
+
+      {posts.reverse().map((post) => (
+        <>
+          <div className="posts">
+            <div link-div>
+              <Link to={`/posts/${post.id}`}>
+                <div className="tittle">
+                  <h3>{post.tittle}</h3>
+                  <p>{post.blog}..</p>
                 </div>
-                
-                </>
-              ))}
+                <div className="image">
+                  <img src={post.image} width="200px" />
+                </div>
+              </Link>
+            </div>
 
-        </div>
-        
-    )
+            <div className="like-com-delete">
+              <h4>
+                {" "}
+                <button
+                  onClick={() => {
+                    fetch(`http://localhost:4000/likePosts`, {
+                      method: "POST",
+                      headers: {
+                        "Content-Type": "application/json",
+                      },
+                      body: JSON.stringify({ postId: post.id }),
+                    })
+                      .then((resp) => resp.json())
+                      .then(() => location.reload());
+                  }}
+                >
+                  ❤️
+                </button>{" "}
+                {post.likes.length} likes
+              </h4>
+              <h4>
+                <button>💬</button> {post.comments.length} comments
+              </h4>
+              <button
+                onClick={() => {
+                  fetch(`http://localhost:4000/posts/${post.id}`, {
+                    method: "DELETE",
+                  })
+                    .then((resp) => resp.json())
+                    .then(() => location.reload());
+                }}
+              >
+                {" "}
+                DELETE{" "}
+              </button>
+            </div>
+          </div>
+        </>
+      ))}
+    </div>
+  );
 }
--- hoxton-node-medium-frontend/src/pages/SinglePostPage.tsx
+++ hoxton-node-medium-frontend/src/pages/SinglePostPage.tsx
@@ -2,94 +2,108 @@ import { ReactNode, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 type Post = {
-    id : number,
-    tittle: string,
-    blog: string,
-    image : string,
-    userId : number
-    likes: []
-    comments:[]
-  }
-  type Comments = {
-    id: number
-    text: string
-    postId: number
-}
+  id: number;
+  tittle: string;
+  blog: string;
+  image: string;
+  userId: number;
+  likes: [];
+  comments: [];
+};
+type Comments = {
+  id: number;
+  text: string;
+  postId: number;
+};
 type Users = {
   id: ReactNode;
-  username : string,
-  image : string,
-  email: string,
-}
+  username: string;
+  image: string;
+  email: string;
+};
 
 export function SinglePostPage() {
   const [post, setPost] = useState<Post | null>(null);
   const params = useParams();
-  const [comments, setComments] = useState<Comments[]>([])
-  const [users, setUsers] = useState<Users[]>([])
-
-  useEffect(() => {
-      fetch(`http://localhost:4000/comments/`)
-        .then((resp) => resp.json())
-        .then((commentsFromServer) => setComments(commentsFromServer));
-    }, []);
+  const [users, setUsers] = useState<Users[]>([]);
 
   useEffect(() => {
     fetch(`http://localhost:4000/posts/${params.id}`)
       .then((resp) => resp.json())
       .then((postFromServer) => setPost(postFromServer));
   }, []);
-  
+
   useEffect(() => {
-  fetch(`http://localhost:4000/users`)
+    fetch(`http://localhost:4000/users`)
       .then((resp) => resp.json())
       .then((usersFromServer) => setUsers(usersFromServer));
-}, [])
+  }, []);
   if (post === null) return <h1>Loading... </h1>;
 
   return (
     <section className="post-detail">
-       
-                <>
-                <div className='posts'>
-                  <div className='tittle'>
-                  <img src={post.image} width="400px" />
-                  <p>User ID {post.userId}</p>
-                    <h3>{post.tittle}</h3>
-                    <p>{post.blog}</p>
-                    <p>{post.blog}</p>
-                    <p>{post.blog}</p>
-                    <p>{post.blog}</p>
-                    <p>{post.blog}</p>
-                    </div>
-                    <div className='image'>
-                    </div>
-                    <div className='like-com-delete'>
-                    <h4> <button
-          onClick={() => {
-            fetch(`http://localhost:4000/likePosts`, {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({ postId: post.id }) ,
-            }).then((resp)=> resp.json()).then(()=> location.reload());
-          }}>❤️</button>{post.likes.length} Likes</h4>
-                    <h4><button>💬{post.comments.length} Comments</button></h4>
-                      <button onClick={()=>{
-                      fetch(`http://localhost:4000/posts/${post.id}`,{
-                        method:"DELETE"
-                      }).then((resp) => resp.json())
-                      .then(() =>
-                    location.reload()
-                      )
-                  
-                    }}> DELETE </button>
-                      </div>
-                </div>
-                </>
-              
-       
+      <>
+        <div className="posts">
+          <div className="tittle">
+            <img src={post.image} width="400px" />
+            <p>User ID {post.userId}</p>
+            <h3>{post.tittle}</h3>
+            <p>{post.blog}</p>
+            <p>{post.blog}</p>
+            <p>{post.blog}</p>
+            <p>{post.blog}</p>
+            <p>{post.blog}</p>
+          </div>
+          <div className="image"></div>
+          <div className="like-com-delete">
+            <h4>
+              {" "}
+              <button
+                onClick={() => {
+                  fetch(`http://localhost:4000/likePosts`, {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ postId: post.id }),
+                  })
+                    .then((resp) => resp.json())
+                    .then(() => location.reload());
+                }}
+              >
+                ❤️
+              </button>
+              {post.likes.length} Likes
+            </h4>
+            <h4>
+              <button>💬</button>
+              {post.comments.length} Comments
+            </h4>
+            <button
+              onClick={() => {
+                fetch(`http://localhost:4000/posts/${post.id}`, {
+                  method: "DELETE",
+                })
+                  .then((resp) => resp.json())
+                  .then(() => location.reload());
+              }}
+            >
+              {" "}
+              DELETE{" "}
+            </button>
+          </div>
+          <form
+            className="comment-form">
+            <input type="text" name="text" placeholder="Comment.." required></input>
+            <button>COMMENT</button>
+          </form>
+          {post.comments.map((comment) => (
+            <div className="comments">
+              <p>💬 {comment.text}</p>
+            </div>
+          ))}
+        </div>
+      </>
     </section>
   );
 }
